7e20b76fec48804b0a9144e76b8dfd6bfc4cfddb
shucheng <shucheng@bd-apaas.com>
2021-01-13 14:08:38 +0800

    wod/node-exporter:v1.0.1

--- .beagle/build.sh
+++ .beagle/build.sh
@@ -6,11 +6,11 @@ mkdir -p dist
 
 cat /etc/resolv.conf
 
-curl -fSL --output dist/promu-0.5.0.linux-amd64.tar.gz https://dl.wodcloud.com/beagle/promu/promu-0.5.0.linux-amd64.tar.gz
-cd dist
-tar zxf promu-0.5.0.linux-amd64.tar.gz
-mv promu-0.5.0.linux-amd64/promu $GOPATH/bin/promu
-cd ..
+# curl -fSL --output dist/promu-0.5.0.linux-amd64.tar.gz https://dl.wodcloud.com/beagle/promu/promu-0.5.0.linux-amd64.tar.gz
+# cd dist
+# tar zxf promu-0.5.0.linux-amd64.tar.gz
+# mv promu-0.5.0.linux-amd64/promu $GOPATH/bin/promu
+# cd ..
 
 export GOARCH=amd64
 make build
